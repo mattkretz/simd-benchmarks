4a5fbdef44a1ab71494df1c1412ebd8c37c37e21
Matthias Kretz <m.kretz@gsi.de>
2024-03-14 18:04:51 +0100

    Use cpufreq/boost when intel_pstate isn't used

--- benchmark-mode.sh
+++ benchmark-mode.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 no_turbo=/sys/devices/system/cpu/intel_pstate/no_turbo
+boost=/sys/devices/system/cpu/cpufreq/boost
 
 write() {
   #echo "write $*"
@@ -15,31 +16,28 @@ write() {
 
 turn_on() {
   echo "enabling benchmark mode (no clock scaling/turbo)"
-  for i in /sys/devices/system/cpu/cpu[0-9]*/cpufreq/scaling_governor; do
+  for i in /sys/devices/system/cpu/cpufreq/policy[0-9]*/scaling_governor; do
     write performance $i
   done
   if test -f $no_turbo; then
     write 1 $no_turbo
-  #else
-    #freq=$(cut -d" " -f1,2 /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies)
-    #freq1=${freq% *}
-    #freq2=${freq#* }
-    #test $(($freq2+1000)) -eq $freq1 && \
-      #echo $freq2 | sudo tee /sys/devices/system/cpu/cpu[0-9]*/cpufreq/scaling_max_freq >/dev/null
+  elif test -f $boost; then
+    write 0 $boost
   fi
 }
 
 turn_off() {
   echo "disabling benchmark mode"
-  for i in /sys/devices/system/cpu/cpu[0-9]*/cpufreq/scaling_governor; do
-    write powersave $i
-  done
+  governor=schedutil
   if test -f $no_turbo; then
     write 0 $no_turbo
-  #else
-    #freq=$(cut -d" " -f1 /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies)
-    #echo $freq | sudo tee /sys/devices/system/cpu/cpu[0-9]*/cpufreq/scaling_max_freq >/dev/null
+    governor=powersave
+  elif test -f $boost; then
+    echo 1 > $boost
   fi
+  for i in /sys/devices/system/cpu/cpufreq/policy[0-9]*/scaling_governor; do
+    write $governor $i
+  done
 }
 
 while (($# > 0)); do
@@ -50,9 +48,8 @@ while (($# > 0)); do
       ;;
     --chown)
       sudo chown $USER \
-        $no_turbo \
-        /sys/devices/system/cpu/cpu[0-9]*/cpufreq/scaling_max_freq \
-        /sys/devices/system/cpu/cpu[0-9]*/cpufreq/scaling_governor
+        $no_turbo $boost \
+        /sys/devices/system/cpu/cpufreq/policy[0-9]*/scaling_governor
       ;;
     on|start) turn_on ;;
     off|stop) turn_off ;;
